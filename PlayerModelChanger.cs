4942dbf2ae4cf3a7ceb6ee67c73a4b8480b5ba3e
samyyc <3356207189@qq.com>
2024-07-06 21:50:45 +0800

    forgot to update version number... :(

--- PlayerModelChanger.cs
+++ PlayerModelChanger.cs
@@ -13,7 +13,7 @@ namespace PlayerModelChanger;
 public partial class PlayerModelChanger : BasePlugin, IPluginConfig<ModelConfig>
 {
     public override string ModuleName => "Player Model Changer";
-    public override string ModuleVersion => "1.5.0";
+    public override string ModuleVersion => "1.5.1";
 
     public override string ModuleAuthor => "samyyc";
     public required ModelConfig Config { get; set; }
@@ -103,7 +103,7 @@ public partial class PlayerModelChanger : BasePlugin, IPluginConfig<ModelConfig>
 
         if (!new string[]{"chat", "centerhtml", "wasd", "interactive"}.Contains(config.MenuType.ToLower())) {
             throw new Exception($"Unknown menu type: {config.MenuType}");
-        }
+        } 
         config.MenuType = config.MenuType.ToLower();
         foreach (var entry in config.Models)
         {
